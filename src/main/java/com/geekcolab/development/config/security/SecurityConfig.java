33b9839a298e21640b2517ffabbb55923589e507
Abdi T Bedaso <atbedaso@gmail.com>
2021-10-23 22:33:15 -0500

    security config

---
+++ src/main/java/com/geekcolab/development/config/security/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.geekcolab.development.config.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
+
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http.cors().and().csrf().disable()
+                .authorizeExchange()
+                .pathMatchers("/food/**").permitAll()
+                .pathMatchers("/").permitAll()
+                .anyExchange().authenticated();
+//                .and()
+//                .oauth2ResourceServer()
+//                .jwt();
+        return http.build();
+    }
+
+}
